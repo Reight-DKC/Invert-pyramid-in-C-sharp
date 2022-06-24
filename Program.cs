3dba104ec086d81c755846afcbb95f5ad6d34d95
Reight-DKC <reightultimate@gmail.com>
2022-06-24 12:14:59 -0600

    Inverse pyramid complete

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Inverse Number Pyramid");
+Console.Write("Number of rows: ");
+
+int rows = Int32.Parse(Console.ReadLine());
+
+for (int i = rows; i >= 1; i--)
+ {   
+ for (int j = i; j >= 1; j--)
+     {
+         Console.Write(j + " ");
+     }
+        Console.WriteLine();
+ }
+
+Console.WriteLine("\nEnd of Program");
